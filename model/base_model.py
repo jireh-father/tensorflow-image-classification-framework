7e6b019c28bf2d34cb3671f062baab08388dcb43
seoilgun <seoilgun@gmail.com>
2018-04-05 00:09:28 +0900

    start

--- model/base_model.py
+++ model/base_model.py
@@ -13,8 +13,8 @@ def build_model(inputs, config, is_training):
     net = tf.layers.conv2d(net, 256, 3, activation=tf.nn.relu, name="conv3")
     net = tf.layers.conv2d(net, 256, 3, activation=tf.nn.relu, name="conv4")
     net = tf.layers.conv2d(net, 128, 3, activation=tf.nn.relu, name="conv5")
-    net = tf.layers.max_pooling2d(net, 3, 2, name="max_pool3")
     end_points["conv5"] = net
+    net = tf.layers.max_pooling2d(net, 3, 2, name="max_pool3")
     net = tf.reshape(net, [-1, int(net.get_shape()[1]) * int(net.get_shape()[2]) * int(net.get_shape()[3])])
     net = tf.layers.dense(net, 512, activation=tf.nn.relu, name="fc1")
     net = tf.layers.dense(net, 256, activation=tf.nn.relu, name="fc2")
