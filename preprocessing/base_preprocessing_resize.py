a4278be88d58aa1b9a40468c60040031a81585af
seoilgun <seoilgun@gmail.com>
2019-05-26 23:48:41 +0900

    gg

--- preprocessing/base_preprocessing_resize.py
+++ preprocessing/base_preprocessing_resize.py
@@ -9,8 +9,8 @@ def preprocessing(image, w, h, config, is_training):
     image = tf.cast(image, tf.float32)
 
     image = tf.expand_dims(image, 0)
-    image = tf.image.resize_image_with_pad(image, w, h)
-    # image = tf.image.resize_bilinear(image, [224, 224], align_corners=False)
+    # image = tf.image.resize_image_with_pad(image, w, h)
+    image = tf.image.resize_bilinear(image, [w, h], align_corners=False)
     image = tf.squeeze(image, [0])
 
     image = tf.divide(image, 255.0)
