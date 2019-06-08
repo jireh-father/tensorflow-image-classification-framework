8179c81349010e350c21a4e7847754814512af14
seoilgun <seoilgun@gmail.com>
2019-06-08 20:35:59 +0900

    gg

--- inference.py
+++ inference.py
@@ -106,10 +106,16 @@ def parse_function(filename):
     return image_decoded
 print(filenames)
 print("inference file count", len(filenames))
-dataset = tf.data.Dataset.from_tensor_slices(filenames)
+
+
+filenames_pl = tf.placeholder(tf.string, shape=[None], name="filenames")
+dataset = tf.data.TFRecordDataset(filenames_pl)
+
+# dataset = tf.data.Dataset.from_tensor_slices(filenames)
 dataset = dataset.map(parse_function, num_parallel_calls=args.num_preprocessing_threads)
 dataset = dataset.batch(args.batch_size)
-iterator = dataset.make_one_shot_iterator()
+# iterator = dataset.make_one_shot_iterator()
+iterator = dataset.make_initializable_iterator()
 next_batch = iterator.get_next()
 
 tf_config = tf.ConfigProto()
@@ -127,15 +133,33 @@ inference_results = {}
 steps = int(nums_samples / args.batch_size)
 if nums_samples % args.batch_size > 0:
     steps += 1
-for i in range(steps):
+    
+
+sess.run(iterator.initializer, feed_dict={filenames_pl: filenames})
+scores = None
+import numpy as np
+while True:
+  try:
     batch_imgs = sess.run(next_batch)
     feed_dict = {inputs: batch_imgs, is_training: False}
     tmp_logits = sess.run(tf.nn.softmax(logits), feed_dict=feed_dict)
-    for j, tmp_logit in enumerate(tmp_logits):
-        top_k_args = tmp_logit.argsort()[-args.top_k:][::-1]
-        top_k_labels = [int(v) for v in list(top_k_args)]
-        top_k_labels_name = [label_map[str(v)] for v in top_k_labels]
-        inference_results[filenames[j + i * args.batch_size]] = [top_k_labels, top_k_labels_name, [float(v) for v in tmp_logit[top_k_labels]]]
-        print(os.path.basename(filenames[j + i * args.batch_size]), [top_k_labels, top_k_labels_name, [float(v) for v in tmp_logit[top_k_labels]]])
+    if scores is None:
+      scores = tmp_logits
+    else:
+      scores = np.concatenate((scores, tmp_logits))
+  except tf.errors.OutOfRangeError:
+    break
+print(scores.shape)
 
-json.dump(inference_results, open(os.path.join(args.log_dir, 'inference_result.json'), "w+"))
+# for i in range(steps):
+#     batch_imgs = sess.run(next_batch)
+#     feed_dict = {inputs: batch_imgs, is_training: False}
+#     tmp_logits = sess.run(tf.nn.softmax(logits), feed_dict=feed_dict)
+#     for j, tmp_logit in enumerate(tmp_logits):
+#         top_k_args = tmp_logit.argsort()[-args.top_k:][::-1]
+#         top_k_labels = [int(v) for v in list(top_k_args)]
+#         top_k_labels_name = [label_map[str(v)] for v in top_k_labels]
+#         inference_results[filenames[j + i * args.batch_size]] = [top_k_labels, top_k_labels_name, [float(v) for v in tmp_logit[top_k_labels]]]
+#         print(os.path.basename(filenames[j + i * args.batch_size]), [top_k_labels, top_k_labels_name, [float(v) for v in tmp_logit[top_k_labels]]])
+#
+# json.dump(inference_results, open(os.path.join(args.log_dir, 'inference_result.json'), "w+"))
